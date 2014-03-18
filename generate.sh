10721806950b28c1c0d04e19fa3f24aa4b3ad418
Andreas Jonsson <mail@andreasjonsson.se>
2014-03-18 11:39:51 +0100

    Updated build-scripts for OSX and Linux.

--- generate.sh
+++ generate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ -d "/Users" && -f "/mach_kernel" ]; then
+if [[ -d "/Users" && -f "/mach_kernel" ]]; then
 	./tools/premake4/premake4.osx xcode3
 else
 	./tools/premake4/premake4.lnx gmake
